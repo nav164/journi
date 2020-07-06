c3f665d1200cff26ca5c539aa54a3180426eb192
nav164 <naveen.deism@gmail.com>
2020-07-07 00:04:51 +0200

    Remove unused import

--- src/main/java/com/journi/challenge/controllers/PurchasesController.java
+++ src/main/java/com/journi/challenge/controllers/PurchasesController.java
@@ -12,7 +12,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.journi.challenge.CurrencyConverter;
